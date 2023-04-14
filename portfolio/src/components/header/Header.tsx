0812b72c9ef1220c3eea62f7d6894e0f758e12c2
Kornull <wolod1987kor@gmail.com>
2023-04-14 20:42:19 +0300

    fix: logo

--- portfolio/src/components/header/Header.tsx
+++ portfolio/src/components/header/Header.tsx
@@ -1,15 +1,37 @@
+import { useEffect, useState } from 'react';
+
+import { GiHamburgerMenu } from 'react-icons/gi';
+
 import { HeaderLogo } from './headerLogo';
 
 import styles from './Header.module.scss';
 
+const aarr = [
+  'lorem',
+  '{list(anchor)}',
+  '{list(anchor)}',
+  '{list(anchor)}',
+  '{list(anchor)}',
+];
 const Header = () => {
+  const [isOpenMenu, setOpenMenu] = useState<boolean>(false);
+
+  const toggleDrawer = () => {
+    setOpenMenu(!isOpenMenu);
+  };
+
+  useEffect(() => {
+    console.log(isOpenMenu, 'use');
+  }, [isOpenMenu]);
+
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
         <HeaderLogo />
-        <div
-          style={{ width: '56px', height: '56px', background: '#000' }}
-        ></div>
+
+        <div className={styles.headerBurger} onClick={toggleDrawer}>
+          {<GiHamburgerMenu className={styles.headerBurgerIcon} />}
+        </div>
       </div>
     </header>
   );
--- portfolio/src/components/header/headerLogo/HeaderLogo.tsx
+++ portfolio/src/components/header/headerLogo/HeaderLogo.tsx
@@ -1,15 +1,11 @@
 import { NavLink } from 'react-router-dom';
 
-import Logo from '/Kornull.svg';
-
 import styles from '../Header.module.scss';
 
 export const HeaderLogo = () => {
   return (
     <NavLink to="/" className={styles.headerLogo}>
-      <svg className={styles.headerLogoIcon}>
-        <use xlinkHref={`${Logo}#kornull`}></use>
-      </svg>
+      <h1 className={styles.headerTitle}>KornUll</h1>
     </NavLink>
   );
 };
