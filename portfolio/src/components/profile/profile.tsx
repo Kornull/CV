0f71d3f35f1ce0621f63e9d200ba29662eccbe3e
Kornull <wolod1987kor@gmail.com>
2023-04-16 16:03:20 +0300

    feat: add about text, bg

--- portfolio/src/components/profile/profile.tsx
+++ portfolio/src/components/profile/profile.tsx
@@ -1,29 +1,31 @@
-import Image from '/public/foto.png';
-
-import styles from './profile.module.scss';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+
+import Image from '/public/foto.png';
+import styles from './profile.module.scss';
 
 const Profile = () => {
   const { t } = useTranslation();
+  const [isOpenText, setOpenText] = useState<boolean>(false);
 
   return (
     <div className={styles.profile}>
       <div className={styles.profileContainer}>
         <div className={styles.profileSammary}>
-          <p style={{ color: '#fff', fontSize: '24px' }}>{t('about')}</p>
-
-          {/* <p style={{ color: '#fff', fontSize: '24px' }}>
-            {`I have one year of experience in web application development (games, landing pages, SPA). A good knowledge of JavaScript basics allows me to quickly learn new technologies and add value to the team.
-My aim is to improve my skills in order to develop a quality and a marketable product.`}
-          </p>
-          <p style={{ color: '#fff', fontSize: '24px' }}>
-            {`Also, I developed many home projects, ranging from pure HTML, CSS and JavaScript landing pages and SPA applications using React, Typescript.
-Acted as a team leader in a 3-person team during the final assignment of the JavaScript course.`}
-          </p> */}
+          <article className={styles.profileSammaryText}>
+            {!isOpenText ? t('about') : t('about-full')}
+            <button
+              className={styles.profileSammaryTextButton}
+              onClick={() => setOpenText(!isOpenText)}
+            >
+              {!isOpenText ? t('buttons.more') : t('buttons.close')}
+            </button>
+          </article>
         </div>
 
         <div className={styles.profileFoto}>
           <img
+            className={styles.profileFotoImg}
             src={Image}
             alt=""
             style={{
