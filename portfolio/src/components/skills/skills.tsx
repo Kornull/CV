86eed0facb35c601b5c4375b084603911bd10da6
Kornull <wolod1987kor@gmail.com>
2023-04-16 19:42:30 +0300

    feat: add run line

--- portfolio/src/components/skills/skills.tsx
+++ portfolio/src/components/skills/skills.tsx
@@ -1,8 +1,34 @@
 import styles from './skills.module.scss';
+import Marquee from 'react-fast-marquee';
+const numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const Skills = () => {
   return (
-    <div className={styles.skill}>Skiiiiiiiiiiiiiiiillllllllllssssssss</div>
+    <div className={styles.skill}>
+      <div className={styles.skillContainer}>
+        <div className={styles.skillMarguee}>
+          <Marquee speed={80}>
+            {numArr.map((num) => (
+              <img
+                style={{ width: 'auto', height: '60px', marginRight: '20px' }}
+                key={num}
+                src={`src/assets/logo/${num}.png`}
+                alt=""
+              />
+            ))}
+            {numArr.map((num) => (
+              <img
+                style={{ width: 'auto', height: '60px', marginRight: '20px' }}
+                key={num}
+                src={`src/assets/logo/${num}.png`}
+                alt=""
+              />
+            ))}
+          </Marquee>
+        </div>
+        <h2>dddd</h2>
+      </div>
+    </div>
   );
 };
 
