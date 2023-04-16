ade5f238ecfe33950b9f232d374b259ad7137f74
Kornull <wolod1987kor@gmail.com>
2023-04-16 16:41:05 +0300

    feat: add summary component

--- portfolio/src/components/profile/profile.tsx
+++ portfolio/src/components/profile/profile.tsx
@@ -1,37 +1,17 @@
-import { useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { ProfileSummary } from './profileSummary';
 
 import Image from '/public/foto.png';
+
 import styles from './profile.module.scss';
 
 const Profile = () => {
-  const { t } = useTranslation();
-  const [isOpenText, setOpenText] = useState<boolean>(false);
-
   return (
     <div className={styles.profile}>
       <div className={styles.profileContainer}>
-        <div className={styles.profileSammary}>
-          <article className={styles.profileSammaryText}>
-            {!isOpenText ? t('about') : t('about-full')}
-            <button
-              className={styles.profileSammaryTextButton}
-              onClick={() => setOpenText(!isOpenText)}
-            >
-              {!isOpenText ? t('buttons.more') : t('buttons.close')}
-            </button>
-          </article>
-        </div>
+        <ProfileSummary />
 
         <div className={styles.profileFoto}>
-          <img
-            className={styles.profileFotoImg}
-            src={Image}
-            alt=""
-            style={{
-              objectFit: 'cover',
-            }}
-          />
+          <img className={styles.profileFotoImg} src={Image} alt="foto" />
         </div>
       </div>
     </div>
