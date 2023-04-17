9c8d04cee2cdfae268dc9b40666e0c0a12cfcf55
Kornull <wolod1987kor@gmail.com>
2023-04-17 13:35:28 +0300

    feat: add mixins

--- portfolio/src/components/tasks/tasks.tsx
+++ portfolio/src/components/tasks/tasks.tsx
@@ -1,5 +1,18 @@
+import { useTranslation } from 'react-i18next';
+
+import styles from './tasks.module.scss';
+import { LanguageQuery } from 'src/types';
+
 const Tasks = () => {
-  return <h1>Tasks</h1>;
+  const { t } = useTranslation();
+
+  return (
+    <div className={styles.tasks}>
+      <div className={styles.tasksContainer}>
+        <h2 className={styles.tasksTitle}>{t(LanguageQuery.PROJECTS)}</h2>;
+      </div>
+    </div>
+  );
 };
 
 export default Tasks;
