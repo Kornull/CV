24039e6f9f0b5a71a6475c1e6d0d7c31397824ea
Kornull <wolod1987kor@gmail.com>
2023-04-18 01:18:06 +0300

    fix: convert img

--- portfolio/src/components/helper/hepler.ts
+++ portfolio/src/components/helper/hepler.ts
@@ -1,6 +1,6 @@
 export const imageNewUrl = (query: number | string): string => {
   if (typeof query === 'number') {
-    return new URL(`/src/assets/logo/${query}.png`, import.meta.url).href;
+    return new URL(`/src/assets/logo/${query}.webp`, import.meta.url).href;
   }
 
   return new URL(`/${query}`, import.meta.url).href;
